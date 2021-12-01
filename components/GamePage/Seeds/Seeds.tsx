8cc1b801bf44c888331d295b801a2a67b11a7015
Juliette <juliette.sageaubriot@gmail.com>
2021-12-01 13:03:49 +0100

    test: fix deploy

--- components/GamePage/Seeds/Seeds.tsx
+++ components/GamePage/Seeds/Seeds.tsx
@@ -3,16 +3,17 @@ import { NavigationContext } from "@components/Context/NavigationProvider";
 
 import styles from './styles.module.scss';
 
-interface ISeeds {}
+interface ISeeds { }
 
-const Seeds = ({  }: ISeeds) => {
+const Seeds = ({ }: ISeeds) => {
     const { choices } = useContext(NavigationContext);
+
 
     return (
         <div className={styles["seeds_container"]}>
             je suis le container des Seeds
 
-            {choices.map((i, idx) => <div key={idx}>je suis la seed combinée de l'étape {idx + 1}, avec le choix {i}</div>)}
+            {choices.map((i:number, idx:number) => <div key={idx}>je suis la seed combinée de l'étape {idx + 1}, avec le choix {i}</div>)}
 
         </div>
     )
