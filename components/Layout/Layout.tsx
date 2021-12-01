6c80a5a28246bb55aefa36d1892f38abea3386d3
Juliette <juliette.sageaubriot@gmail.com>
2021-12-01 14:38:51 +0100

    feat: update headtag component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,17 +1,20 @@
-import SoundInputs from "@components/SoundInputs/SoundInputs";
 import React from "react";
+import HeadTag from "@components/HeadTag/HeadTag";
+import SoundInputs from "@components/SoundInputs/SoundInputs";
 
 import styles from './styles.module.scss'
 
 interface ILayout {
     children: any,
-    page: string
+    page: string,
+    titleSEO: string
 }
 
-const Layout = ({ children, page }: ILayout) => {
+const Layout = ({ children, page, titleSEO }: ILayout) => {
     return (
         <div className={`${styles['layout_container']}`}>
             <div className={`${styles['layout__inner']}`}>
+                <HeadTag title={titleSEO}/>
                 <div className={`${styles['content-wrapper']} ${styles[`${page}-content`]}`}>
                     {children}
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import HeadTag from "@components/HeadTag/HeadTag";
 import HomePage from "@components/HomePage/HomePage";
 import Layout from "@components/Layout/Layout";
 
 const Home = () => {
   return (
-    <Layout page="home">
-      <HeadTag title="LaBotaNik'" />
+    <Layout page="home" titleSEO="LaBotaNik'">
       <HomePage />
     </Layout>
   )
