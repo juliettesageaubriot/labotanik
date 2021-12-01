a77639bbf698d01a0f7e6083099632ede42759b7
MorganeSauneuf <morgane.sauneuf@gmail.com>
2021-12-01 10:56:59 +0100

    feat: structure homepage & endpage

---
+++ components/HomePage/HomePage.tsx
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from "react";
+import { NavigationContext } from "@components/Context/NavigationProvider";
+import Link from 'next/link';
+
+import styles from './styles.module.scss';
+
+const HomePage = () => {
+    const { userName, setUserName } = useContext(NavigationContext);
+
+    useEffect(() => console.log(userName), [userName])
+
+    return (
+        <div className={styles["home_container"]}>
+            <h1>LaBotaNik</h1>
+            <input
+                type="text"
+                value={userName}
+                placeholder="Inscris ton nom ici..."
+                onChange={(e) => setUserName(e.target.value)}
+            />
+
+            <Link href="/game">
+                <a>commencer le jeu</a>
+            </Link>
+        </div>
+    )
+}
+
+export default HomePage
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { NavigationContext } from "@components/Context/NavigationProvider";
 import HeadTag from "@components/HeadTag/HeadTag";
-// import Link from 'next/link';
 import GamePage from "@components/GamePage/GamePage";
 
 const Game = () => {
@@ -12,11 +11,6 @@ const Game = () => {
     return (
         <>
             <HeadTag title="LaBotaNik' - Jeu" />
-            {/* <h1>Hello world - Game Page</h1>
-            <Link href="/end">
-                <a>fin du jeu</a>
-            </Link> */}
-
             <GamePage />
         </>
     )
