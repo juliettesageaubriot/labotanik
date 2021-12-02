e5144ef51fcda46e34e39c7695608f60bac8c48e
Juliette <juliette.sageaubriot@gmail.com>
2021-12-02 22:26:22 +0100

    feat: update sounds

--- data/sounds.tsx
+++ data/sounds.tsx
@@ -3,18 +3,24 @@ export const sounds = [
         id: 'cat-died',
         play: false,
         loop: false,
-        path: '/assets/sounds/one-time/cats_death_bells_tears.mp3'
+        path: '/assets/sounds/one-time/cats_death_bells_tears.mp3',
+        alreadyPlaying: false
     },
     {
         id: 'clim',
         play: false,
-        loop: false,
-        path: '/assets/sounds/one-time/bip_clim_fans.mp3'
+        loop: true,
+        // path: [
+        //     '/assets/sounds/one-time/bip_clim_fans.mp3',
+        //     '/assets/sounds/loop/fans.mp3'],
+        path: '/assets/sounds/loop/fans.mp3',
+        alreadyPlaying: false
     },
     {
         id: 'crackles',
         play: false,
         loop: false,
-        path: '/assets/sounds/one-time/car_crashed.mp3'
+        path: '/assets/sounds/one-time/car_crashed.mp3',
+        alreadyPlaying: false
     }
 ]
