6ecc321784139c1219480a2d3d2a048b757a764f
Juliette <juliette.sageaubriot@gmail.com>
2021-12-03 12:01:50 +0100

    fix: fix volume and mute of the video

--- components/CinePage/CinePage.tsx
+++ components/CinePage/CinePage.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useRef, useContext, useEffect } from "react";
 import { useRouter } from 'next/router'
+import { NavigationContext } from "@components/Context/NavigationProvider";
 
 import styles from './styles.module.scss';
 
 const CinePage = () => {
+    const { mutedGlobalVolume, soundGlobalVolume } = useContext(NavigationContext);
     const router = useRouter()
+
+    useEffect(() => {
+        const vid:any = document.getElementById("video")
+        vid.volume = soundGlobalVolume
+    }, [soundGlobalVolume])
 
     return (
         <div className={styles["cine_container"]}>
-            <video autoPlay onEnded={() => router.push('/game')}>
+            <video id="video" muted={!!mutedGlobalVolume ? true : false} autoPlay onEnded={() => router.push('/game')}>
                 <source src="/assets/videos/cinematik.mp4" type="video/mp4" />
             </video>
         </div>
